c9ac384dcf5051fe0540b2f2eb6d1bdee4e93ed0
nicktho <matenaar92@gmail.com>
2020-11-26 16:00:24 +1100

    add element script

---
+++ element-custom-locator.perf.ts
@@ -0,0 +1,45 @@
+import { step, TestSettings, By, Locator } from '@flood/element'
+import { BaseLocator } from '@flood/element-core/dist/src/page/Locator'
+import { LocatorBuilder } from '@flood/element-core/dist/src/page/types'
+
+class CustomBuilder implements LocatorBuilder {
+	constructor(public query: string) {}
+
+	get pageFuncArgs(): any[] {
+		return [this.query]
+	}
+
+	get pageFunc(): LocatorBuilder['pageFunc'] {
+		return (query: string) => {
+			return document.querySelector(query)
+		}
+	}
+
+	get pageFuncMany(): LocatorBuilder['pageFuncMany'] {
+		return (query: string) => {
+			return document.querySelectorAll(query)
+		}
+	}
+}
+
+const customLocator = (query: string): Locator =>
+	new BaseLocator(new CustomBuilder(query), `customLocator(${query})`)
+
+export const settings: TestSettings = {
+	loopCount: 1,
+	waitUntil: 'visible',
+}
+
+export default () => {
+	step('Start', async browser => {
+		await browser.visit('https://challenge.flood.io')
+	})
+
+	step('Find with By', async browser => {
+		await browser.findElement(By.css('#new_challenger > input.btn.btn-xl.btn-default'))
+	})
+
+	step('Find with customLocator', async browser => {
+		await browser.findElement(customLocator('#new_challenger > input.btn.btn-xl.btn-default'))
+	})
+}
